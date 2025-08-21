8d1eed5059e72be9c43a93b123373814fd219032
Vaibhav Vats <vats88690@gmail.com>
2025-08-21 09:45:38 +0530

    sum of 2 number

---
+++ 20August/sumOfTwoNumber.java
@@ -0,0 +1,11 @@
+class sumOfTwoNumber{
+    public static void main(String[] args) {
+        int a=100;
+        int b=20;
+        if(a>b){
+            System.out.println("A is greatest and value of 'a' is " +a);
+        }else{
+            System.out.println("B is greatest and value of 'b' is "+b);
+        }
+    }
+}
