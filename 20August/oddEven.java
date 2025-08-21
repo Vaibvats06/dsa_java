f7bc6396004b99be396caf358314b5c9898a2cea
Vaibhav Vats <vats88690@gmail.com>
2025-08-21 10:09:29 +0530

    add oddEven

---
+++ 20August/oddEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class oddEven {
+    public static void main(String args[]){
+         System.out.println("Enter any odd/even number");
+        Scanner sc= new Scanner(System.in);
+        int number=sc.nextInt();
+        if(number%2==1){
+            System.out.println("It is odd");
+        }else{
+            System.out.println("It is even");
+        }
+    }
+    
+}
