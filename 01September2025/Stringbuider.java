fed1bdbc086ee343d078e94c4380418246371f6f
Vaibhav Vats <vats88690@gmail.com>
2025-09-01 08:05:45 +0530

    String standard form first letter uppercase

--- 01September2025/Stringbuider.java
+++ 01September2025/Stringbuider.java
@@ -1,9 +1,25 @@
 public class Stringbuider{
-    public static void main(String[] args){
+    public static void Upper(String str){
+        str=str.toLowerCase().trim();
         StringBuilder sb=new StringBuilder("");
-        for(char ch='a';ch<='z';ch++){
-            sb.append(ch);
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i-1)==' '){
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append((str.charAt(i)));
+
+            }
         }
         System.out.println(sb);
+
+        
+    }
+
+    public static void main(String[] args){
+        String str="    my name is vAibhav vats and i want take a intership.";
+        Upper(str);
+        
     }
 }
