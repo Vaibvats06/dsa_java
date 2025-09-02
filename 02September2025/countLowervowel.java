8ff44bcfd942dcb55f68fd75f76e3539b6bf0691
Vaibhav Vats <vats88690@gmail.com>
2025-09-02 13:19:02 +0530

    Add pratice q1 of string practice set

---
+++ 02September2025/countLowervowel.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class countLowervowel{
+    public static void countLower(String str)
+    {
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        String Str=sc.nextLine();
+        countLower(Str);
+
+    }
+}
